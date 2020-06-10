50e8bce686b6431c6db1c5d2dd9137967c0ceff2
pastukhVitalii <pastukh.v.7@gmail.com>
2020-06-10 13:54:58 +0300

    add preloader

---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import preloader from "../../img/preloader5.svg";
+
+let Preloader = () => {
+  return <div>
+    <img src={preloader} alt='preloader'/>
+  </div>
+}
+
+export default Preloader;
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,32 +1,36 @@
 import React from "react";
 import css from './Users.module.css';
-import * as axios from 'axios';
 import userPhoto from '../../img/profile-avatar-png-4.png';
 
-class Users extends React.Component {
 
-   componentDidMount() {
-     axios.get("https://social-network.samuraijs.com/api/1.0/users")
-       .then(response => {
-         this.props.setUsers(response.data.items);
-       });
-   }
+let Users = (props) => {
+  let pagesCount = Math.ceil (props.totalUsersCount / props.pageSize);
 
-  render() {
+  let pages = [];
+  for (let i=1; i <= pagesCount; i++) {
+    pages.push(i);
+  }
+
   return <div>
+    <div>
+      { pages.map( p => {
+        return <span className={props.currentPage ===  p && css.selectedPage }
+                     onClick={(e) => { props.onPageChanged(p); }}>{` ${p}`}</span>
+      })}
+    </div>
     {
-      this.props.users.map(u => <div key={u.id}>
+      props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={css.userPhoto}/>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={css.userPhoto} alt='user'/>
                     </div>
                     <div>
                         {u.followed
                           ? <button onClick={() => {
-                            this.props.unfollow(u.id)
+                            props.unfollow(u.id)
                           }}>Unfollow</button>
                           : <button onClick={() => {
-                            this.props.follow(u.id)
+                            props.follow(u.id)
                           }}>Follow</button>}
 
                     </div>
@@ -44,7 +48,6 @@ class Users extends React.Component {
       </div>)
     }
   </div>
-}
 
 }
 
