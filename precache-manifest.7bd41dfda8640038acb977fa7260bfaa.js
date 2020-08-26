3b6a2c0070dc71fafc84f69c5479b19977ea56ab
pastukhVitalii <pastukh.v.7@gmail.com>
2020-08-26 20:00:17 +0300

    Updates

---
+++ precache-manifest.7bd41dfda8640038acb977fa7260bfaa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f74b7d7548fdd239504fe018a1607774",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "9c01a2a8b7b9644040c7",
+    "url": "/social-network/static/css/main.911d947e.chunk.css"
+  },
+  {
+    "revision": "1e463de43824e21a625a",
+    "url": "/social-network/static/js/2.0cef6838.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/social-network/static/js/2.0cef6838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c01a2a8b7b9644040c7",
+    "url": "/social-network/static/js/main.0197ce28.chunk.js"
+  },
+  {
+    "revision": "08c8d3d8a99eb3191bad",
+    "url": "/social-network/static/js/runtime-main.5bdbdf7e.js"
+  },
+  {
+    "revision": "3a7620102dd9905dedd83d4d5ccfc9f1",
+    "url": "/social-network/static/media/preloader5.3a762010.svg"
+  },
+  {
+    "revision": "2a0429f4d6f25b7315d6e91e4a618041",
+    "url": "/social-network/static/media/profile-avatar-png-4.2a0429f4.png"
+  }
+]);
